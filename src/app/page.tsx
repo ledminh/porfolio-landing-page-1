5e9bca6aed4d2f126b750156871f4f76c138f043
Minh Le <minh.le@ledminh.dev>
2024-05-12 21:10:14 -0400

    Add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import HeroImage from "@/components/Home/HeroImage";
+import SideMenu from "@/components/Home/SideMenu";
+import Accomplishments from "@/components/Home/Accomplishments";
+import MyStory from "@/components/Home/MyStory";
+import VideoResume from "@/components/Home/VideoResume";
+import MyServices from "@/components/Home/MyServices";
+import PricePlans from "@/components/Home/PricePlans";
+import Testimonials from "@/components/Home/Testimonials";
+import LastestPublications from "@/components/Home/LastestPublications";
+import Copyright from "@/components/Home/Copyright";
 
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <HeroImage />
+      <SideMenu />
+      <Accomplishments />
+      <MyStory />
+      <VideoResume />
+      <MyServices />
+      <PricePlans />
+      <Testimonials />
+      <LastestPublications />
+      <Copyright />
+    </>
+  );
 }
+
+// https://preview.themeforest.net/item/trueman-personal-portfolio-react-nextjs-template/full_screen_preview/50637184?_ga=2.37967718.435992495.1715492035-621114683.1715492035&_gac=1.128469502.1715492035.CjwKCAjwrvyxBhAbEiwAEg_KgrpQaoDKzKpvj_fpT2UEONJ6foGSHr7XO-OzzdO9onO_T4Q1msBjBRoC0twQAvD_BwE
---
+++ src/components/Home/PricePlans/index.tsx
@@ -0,0 +1,3 @@
+export default function PricePlans() {
+  return <>Price Plans</>;
+}
